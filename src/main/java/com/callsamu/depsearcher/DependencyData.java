0c0d792498a6a729e079f59c1d44a95ff31d769d
callsamu <samuelbree@gmail.com>
2024-08-13 16:41:14 -0300

    feat: properly configure desserialization of DependencyData record

--- src/main/java/com/callsamu/depsearcher/DependencyData.java
+++ src/main/java/com/callsamu/depsearcher/DependencyData.java
@@ -1,7 +1,13 @@
 package com.callsamu.depsearcher;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "dependency")
 public record DependencyData(
-	String groupId, 
-	String artifactId, 
-	String version
+	@JsonProperty("g") String groupId,
+	@JsonProperty("a") String artifactId,
+	@JsonProperty("version") String version
 ){};
