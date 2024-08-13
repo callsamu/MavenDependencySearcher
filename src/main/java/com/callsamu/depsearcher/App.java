03f794a8e70a9a70c0c596730fdb1f7f5f7c57d9
callsamu <samuelbree@gmail.com>
2024-08-13 16:34:13 -0300

    feat: use App class to implement basic CLI with PicoCLI

--- src/main/java/com/callsamu/depsearcher/App.java
+++ src/main/java/com/callsamu/depsearcher/App.java
@@ -1,8 +1,59 @@
 package com.callsamu.depsearcher;
 
+import java.net.http.HttpClient;
+import java.util.concurrent.Callable;
 
-public class App {
-    public static void main( String[] args ) {
-		System.out.println("haha");
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+@Command(
+	name = "mvndsearch",
+	version = "0.0.1",
+	mixinStandardHelpOptions = true,
+	description = "Searchs for dependencies in Maven Central"
+)
+public class App implements Callable<Integer> {
+	@Option(names = {"-g", "--group"}, description = "Group ID")
+	private String group;
+
+	@Option(names = {"-a", "--artifact"}, description = "Artifact ID")
+	private String artifact;
+
+	@Option(names = {"-q", "--query"}, description = "Any Query")
+	private String query;
+
+	@Override
+	public Integer call() throws Exception {
+		MavenCentralAPIClient client = new MavenCentralAPIClient(
+			"https://search.maven.org/",
+			"/solrsearch/select",
+			HttpClient.newHttpClient()
+		);
+
+		final XmlMapper mapper = new XmlMapper();
+		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+		try {
+			final DependencyData data = client.get(this.group, this.artifact);
+			final String r = mapper.writeValueAsString(data);
+			System.out.println(r);
+
+			return 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 1;
+		}
+	}
+
+
+    public static void main(String... args)  {
+		int exitCode = new CommandLine(new App()).execute(args);
+		System.exit(exitCode);
     }
 }
