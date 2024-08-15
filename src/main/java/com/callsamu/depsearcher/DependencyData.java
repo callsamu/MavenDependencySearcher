b91f3829c5089f783fb266fd565f6cc00c9ce0a1
callsamu <samuelbree@gmail.com>
2024-08-15 12:50:44 -0300

    feat: add XML serialization metadata to DependencyData record

--- src/main/java/com/callsamu/depsearcher/DependencyData.java
+++ src/main/java/com/callsamu/depsearcher/DependencyData.java
@@ -2,12 +2,21 @@ package com.callsamu.depsearcher;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JacksonXmlRootElement(localName = "dependency")
 public record DependencyData(
-	@JsonProperty("g") String groupId,
-	@JsonProperty("a") String artifactId,
-	@JsonProperty("version") String version
+	@JsonProperty("g") 
+	@JacksonXmlProperty(localName = "groupId") 
+	String groupId,
+
+	@JsonProperty("a") 
+	@JacksonXmlProperty(localName = "artifactId") 
+	String artifactId,
+
+	@JsonProperty("latestVersion") 
+	@JacksonXmlProperty(localName = "version") 
+	String version
 ){};
