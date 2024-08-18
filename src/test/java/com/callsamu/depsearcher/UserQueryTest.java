2e29d59845d8d23ca42369bf32604211c1cc214e
callsamu <samuelbree@gmail.com>
2024-08-17 22:02:08 -0300

    fix: check cases where version is empty on UserQuery test

--- src/test/java/com/callsamu/depsearcher/UserQueryTest.java
+++ src/test/java/com/callsamu/depsearcher/UserQueryTest.java
@@ -1,6 +1,8 @@
 package com.callsamu.depsearcher;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,24 +11,28 @@ public class UserQueryTest {
 	public void shouldParseClassQuery() throws Exception {
 		final UserQuery q = UserQuery.fromString("com.google.inject.Guice");
 		assertEquals(q.toAPIQueryParam(), "c:com.google.inject.Guice");
+		assertTrue(q.getVersion().isEmpty());
 	}
 
 	@Test
 	public void shouldParseGroupAndArtifactQuery() throws Exception {
 		final UserQuery q = UserQuery.fromString("com.google.inject:guice");
 		assertEquals(q.toAPIQueryParam(), "g:com.google.inject AND a:guice");
+		assertTrue(q.getVersion().isEmpty());
 	}
 
 	@Test
 	public void shouldParseFullTextQuery() throws Exception {
 		final UserQuery q = UserQuery.fromString("google guice");
 		assertEquals(q.toAPIQueryParam(), "google guice");
+		assertTrue(q.getVersion().isEmpty());
 	}
 
 	@Test
 	public void shouldParseClassWithVersionQuery() throws Exception {
 		final UserQuery q = UserQuery.fromString("com.google.inject:guice:3.0");
 		assertEquals(q.toAPIQueryParam(), "g:com.google.inject AND a:guice");
+		assertFalse(q.getVersion().isEmpty());
 		assertEquals(q.getVersion().get(), "3.0");
 	}
 
@@ -34,6 +40,7 @@ public class UserQueryTest {
 	public void shouldParseGroupAndArtifactWithVersionQuery() throws Exception {
 		final UserQuery q = UserQuery.fromString("com.google.inject:guice:3.0");
 		assertEquals(q.toAPIQueryParam(), "g:com.google.inject AND a:guice");
+		assertFalse(q.getVersion().isEmpty());
 		assertEquals(q.getVersion().get(), "3.0");
 	}
 }
